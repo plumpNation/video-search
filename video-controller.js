25c0dd1e70bc0686ff203401feea84592baed9a3
Gavin King <plumpnation.king@pearson.com>
2015-02-12 20:57:57 +0100

    First commit after fork

---
+++ video-controller.js
@@ -0,0 +1,53 @@
+(function () {
+    'use strict';
+
+    var VideosController = function ($scope, $http, $log, YoutubeService, VideosService) {
+
+        init();
+
+        function init() {
+            $scope.youtube = VideosService.getYoutube();
+            $scope.results = VideosService.getResults();
+            $scope.upcoming = VideosService.getUpcoming();
+            $scope.history = VideosService.getHistory();
+            $scope.playlist = true;
+        }
+
+        function onYoutubeServiceLoad(data) {
+            VideosService.listResults(data);
+            $log.info(data);
+        }
+
+        function onYoutubeServiceError(error) {
+            $log.error('Youtube error', error);
+        }
+
+        $scope.launch = function (id, title) {
+            VideosService.launchPlayer(id, title);
+            VideosService.archiveVideo(id, title);
+            VideosService.deleteVideo($scope.upcoming, id);
+            $log.info('Launched id:' + id + ' and title:' + title);
+        };
+
+        $scope.queue = function (id, title) {
+            VideosService.queueVideo(id, title);
+            VideosService.deleteVideo($scope.history, id);
+            $log.info('Queued id:' + id + ' and title:' + title);
+        };
+
+        $scope.delete = function (list, id) {
+            VideosService.deleteVideo(list, id);
+        };
+
+        $scope.search = function () {
+            YoutubeService.search(this.query)
+                .then(onYoutubeServiceLoad, onYoutubeServiceError);
+        }
+
+        $scope.tabulate = function (state) {
+            $scope.playlist = state;
+        }
+    };
+
+    angular.module('VideoSearch').controller('VideosController', VideosController);
+}(window.angular));
---
+++ youtube-service.js
@@ -0,0 +1,45 @@
+(function (app) {
+    'use strict';
+
+    var youtubeApiUrl = 'https://www.googleapis.com/youtube/v3/search',
+
+        YoutubeService = function ($resource, $log, CONFIG) {
+            var _maxResults = '8',
+                youtubeApi = $resource(CONFIG.youtubeApiUrl);
+
+            /**
+             * @param {number} numResults
+             */
+            this.setMaxResults = function (numResults) {
+                _maxResults = numResults.toString();
+            };
+
+            /**
+             * @param {string} query
+             * @return {object} $resource
+             */
+            this.search = function (query) {
+                return youtubeApi.get({
+                    params: {
+                        q           : query,
+                        key         : CONFIG.googleApiKey,
+                        type        : 'video',
+                        maxResults  : _maxResults,
+                        part        : 'id,snippet',
+
+                        fields: [
+                            'items/id',
+                            'items/snippet/title',
+                            'items/snippet/description',
+                            'items/snippet/thumbnails/default',
+                            'items/snippet/channelTitle'
+                        ].join(',')
+                    }
+                }).$promise;
+            };
+        };
+
+    angular.module('VideoSearch')
+        .service('YoutubeService', YoutubeService);
+
+}(window.angular));
